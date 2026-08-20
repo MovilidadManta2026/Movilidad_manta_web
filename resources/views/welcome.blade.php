890bb2e3eb378865bd11dd4909a36bc46d1d3e95
MovilidadManta2026 <tics@movilidadmanta.gob.ec>
2026-08-20 17:06:51 -0500

    cambio de nombre del cliped

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Portal ciudadano de la Alcaldía de Manta">
-    <title>Manta | Alcaldía Ciudadana</title>
+    <title>Manta | Movilidad</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,800,900" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
